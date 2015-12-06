c8d63841f5811b03affc42c500421f5786337ed2
alea qawiyy <aq1984@gmail.com>
2015-12-06 14:24:15 -0500

    added password encryption user.js

--- models/user.js
+++ models/user.js
@@ -2,4 +2,9 @@ require("../db/schema")
 var mongoose = require('mongoose')
 
 var UserModel = mongoose.model("User")
+//encrypt password 
+User.methods.encrypt = function(password) {
+   return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+ };
+
 module.exports = UserModel
