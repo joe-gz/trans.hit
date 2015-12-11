18fd7550b63f4c63eb744423bda0f56e4e4a6953
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2015-12-11 10:10:37 -0500

    refactors delete and adds edit button

--- public/js/views/commentView.js
+++ public/js/views/commentView.js
@@ -10,15 +10,58 @@ CommentView.prototype = {
     console.log("comment"+this.comments.id);
     var commentText = $("<div class = "+this.comments.id+">"+"<p>" + this.comments.text + "</p>"+
     "<button class='deleteComment'>Delete Comment</button>"+"</div>");
-    var deleteButton = self.$el.find(".deleteComment");
-    console.log(commentText);
-    console.log(deleteButton);
 
-    commentText.on("click", function() {
-        console.log("clicked?");
-        self.comments.destroy().then(function() { commentText.fadeOut()});
+    var commentText = $("<div class = "+this.comments.id+">"+"<p>" + this.comments.text + "</p>"+"</div>");
+
+    self.commentAppend(commentText);
+    // var deleteButton = self.$el.find(".deleteComment");
+    // console.log(commentText);
+    // console.log(deleteButton);
+    //
+    // commentText.on("click", function() {
+    //   console.log("clicked?");
+    //   self.comments.destroy().then(function() { commentText.fadeOut()});
+    // });
+    return(commentText)
+  },
+  commentAppend: function(commentText) {
+    var self = this;
+    var editButton = $("<button class='editComment'>Edit Comment</button>");
+    var deleteButton = $("<button class='deleteComment'>Delete Comment</button>"+"</div>");
+
+    editButton.on("click", function() {
+      console.log("clicked?");
+      self.renderEditForm();
     });
 
-    return(commentText)
+    deleteButton.on("click", function() {
+      console.log("clicked?");
+      self.comments.destroy().then(function() { commentText.fadeOut()});
+    });
+
+    commentText.append(editButton);
+    commentText.append(deleteButton);
+
+  },
+  renderEditForm: function() {
+    console.log("render");
+    var self = this;
+    self.commentEditTemplate(self.comments);
+
+    self.$el.find(".updateComment").on("click", function() {
+      self.editComment();
+    });
+    console.log("anything happen");
+  },
+  commentEditTemplate: function() {
+    console.log("edit form?");
+    var comment = this.comments;
+    var html = $("<div class='comments'>");
+    html.append("<input name='name' value='" + comment.text + "'>");
+    html.append("<button class='updateComment'>Update Comment</button>");
+    return(html);
+  },
+  editComment: function(){
+    console.log("edit??");
   }
 }
